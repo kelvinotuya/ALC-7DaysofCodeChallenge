9738b90fa3983b634fd308242ecd0487ece16e5e
Kelvin Otuya <ifeanyiotuya@gmail.com>
2018-06-30 17:10:12 +0100

    add promise.js & fetch.js for old browser support

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+if (!window.Promise) {
+	window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
 
 	navigator.serviceWorker
-		.register('./sw.js', { scope: './' })
+		.register('./sw.js')
 		.then(function (registration) {
 			console.log("Service Worker is Registered");
 		})
--- sw.js
+++ sw.js
@@ -1,14 +1,4 @@
-let cacheName = 'currency-calc-v1';
-let cacheFiles = [
-	'./',
-	'./index.html',
-	'./js/idb.js',
-	'./js/script.js',
-	'./js/index-db.js',
-	'./css/style.css',
-	'https://fonts.googleapis.com/css?family=Roboto:400,700',
-	'./js/script.js'
-]
+const cacheName = 'static-cache-v1';
 
 
 self.addEventListener('install', function(e) {
@@ -16,29 +6,39 @@ self.addEventListener('install', function(e) {
 
 	e.waitUntil(
 		caches.open(cacheName).then(function(cache) {
-			console.log("[ServiceWorker] Caching cacheFiles");
-			return cache.addAll(cacheFiles);
+			console.log("[ServiceWorker] Precaching App Shell");
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/js/script.js',
+				'/css/style.css',
+				'/js/idb.js',
+				'/js/index.js',
+				'/js/promise.js',
+				'/js/fetch.js',
+				'https://fonts.googleapis.com/css?family=Roboto:400,700'
+			]);
 		})
 	)
 })
 
 
 self.addEventListener('activate', function (e) {
-	console.log("[ServiceWorker] Activated")
+	console.log("[ServiceWorker] Activated", e);
+	return self.client.claim();
+	// e.waitUntil(
+	
+	// 	caches.keys().then(function (cacheNames) {
+	// 		return Promise.all(cacheNames.map(function (thisCacheName) {
 
-	e.waitUntil(
-		//createDB()
-		caches.keys().then(function (cacheNames) {
-			return Promise.all(cacheNames.map(function (thisCacheName) {
+	// 			if (thisCacheName !== cacheName) {
 
-				if (thisCacheName !== cacheName) {
-
-					console.log("[ServiceWorker] Removing Cache Files from ", thiscacheName);
-					return caches.delete(thiscacheName);
-				}
-			}));
-		})
-	);
+	// 				console.log("[ServiceWorker] Removing Cache Files from ", thiscacheName);
+	// 				return caches.delete(thiscacheName);
+	// 			}
+	// 		}));
+	// 	})
+	// );
 });
 
 self.addEventListener('fetch', function (e) {
@@ -46,17 +46,13 @@ self.addEventListener('fetch', function (e) {
 	
 	e.respondWith(
 		//To respond with an entry from the cache if there is one, else fetch from the network.
-		caches.match(e.request).then(function (response) {
-			if (response) return response;
-			return fetch(e.request);
+      caches.match(e.request).then(function (response) {
+			if (response) {
+				return response;
+			} else {
+				return fetch(e.request);
+			}
 		})
 	);
-	const requestUrl = new URL(e.request.url);
 
-	if (requestUrl.origin === location.origin) {
-		if (requestUrl.pathname === './') {
-			e.respondWith(cashes.match('./skeleton'));
-			return;
-		}
-	}
 }); 
